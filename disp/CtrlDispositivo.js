db631f35e464a6301a1d1b60451d24635c778fbb
luisefiot <82477066+luisefiot@users.noreply.github.com>
2021-04-13 18:47:57 -0500

    Add files via upload

--- disp/CtrlDispositivo.js
+++ disp/CtrlDispositivo.js
@@ -31,7 +31,7 @@ const ctSondea = document.
 const dispositivoId = prompt(
   "Identificador del Dispositivo",
   "iot1");
-const ID_PROYECTO = "luisefiot-c6ad4";
+const ID_PROYECTO = "angelpereziot-b1ce5";
 const URL_SALIDA =
   urlDeDocumento(ID_PROYECTO,
     "Salida", dispositivoId);
