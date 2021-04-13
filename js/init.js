63c400306d0623049ea99503a3a77578cd8f1f69
luisefiot <82477066+luisefiot@users.noreply.github.com>
2021-04-13 16:59:40 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  apiKey: "AIzaSyAsANp13geFjjSJh1WuyDtHoDCObsVPgOo",
+    authDomain: "luisefiot-c6ad4.firebaseapp.com",
+    projectId: "luisefiot-c6ad4",
+    storageBucket: "luisefiot-c6ad4.appspot.com",
+    messagingSenderId: "1025595939091",
+    appId: "1:1025595939091:web:c608449e5cf1a66c6d62e6"
+
 });
